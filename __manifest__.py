2063f4cfc46c0a08eeb778e4d1110af2d410482c
MatteoSala97 <matteo111997@gmail.com>
2024-09-10 14:23:53 +0000

    fixed names

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
     # -*- coding: utf-8 -*-
 {
-    'name': "Test Module",
+    'name': "Test Model",
 
     'summary': """
         Primo modulo di test""",
@@ -9,13 +9,13 @@
         Descrizione del modulo di test
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Matteo Sala",
+    'website': "admin@admin.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
